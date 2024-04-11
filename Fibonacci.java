6c46c052016466adc4913391bc2951ce19eb6085
Arthur-H-Silva <146857255+Arthur-H-Silva@users.noreply.github.com>
2024-04-11 07:58:30 -0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,7 +2,7 @@ package condicional;
 
 import java.util.Scanner;
 
-public class fibonacci {
+public class Fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
